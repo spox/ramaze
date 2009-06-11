f3c0f6475722f8e0ab5c3e6891812b0bce779b9a
spox <spox@modspox.com>
2009-06-11 15:35:35 -0700

    fixed view for Markaby support

--- lib/ramaze/view/markaby.rb
+++ lib/ramaze/view/markaby.rb
@@ -1,19 +1,15 @@
 require 'builder'
 require 'markaby'
 
-class MarkablyTemplator < BlankSlate
-    def self.process(string, args)
-        template = ::Markaby::Template.new(string)
-        return template.render(args)
-    end
-end
-
 module Ramaze
     module View
         module Markaby
+            # TODO: Use View.compile so we get caching support
             def self.call(action, string)
-                return if string.empty?
-                return MarkablyTemplator.process(string, action.variables), 'text/html'
+                return string if !string.is_a?(String) || string.empty?
+                builder = ::Markaby::Builder.new(action.variables, action.instance)
+                builder.instance_eval(string)
+                return builder.to_s, 'text/html'
             end
         end
     end
